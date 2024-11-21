3583529c8120f5c537f4abd598324188fd94f437
Khalid El Hilali <132703317+NightmareJx@users.noreply.github.com>
2024-11-21 15:35:52 +0100

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "https://shop-click-gfhymgkfu-nightmarejxs-projects.vercel.app/api/v2";
+export const server = "https://shop-click-theta.vercel.app/api/v2";
 
-export const backend_url = "https://shop-click-gfhymgkfu-nightmarejxs-projects.vercel.app";
+export const backend_url = "https://shop-click-theta.vercel.app/";
 
 
