455d4175cbb00253ed1d3a371483d6e24351b85e
Khalid El Hilali <132703317+NightmareJx@users.noreply.github.com>
2024-11-21 14:42:09 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://shop-click-app.vercel.app/',
   credentials: true
 }));
 
